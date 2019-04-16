9a584bba9b6e21cb3ebd2ff350396ac8f124b99f
unknown <noah.stoeckel@gmail.com>
2019-04-16 15:27:23 -0400

    adds testing to display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "react-testing-library";
+
+import Dashboard from "./Dashboard";
+describe("<Dashboard/>", () => {
+  it("renders without crashing", () => {
+    // Render new instance in every test to prevent leaking state
+    render(<Dashboard />);
+  });
+
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />); // generates a DOM tree
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, cleanup } from "react-testing-library";
+afterEach(cleanup);
+
+import Display from "./Display";
+
+describe("<Display/>", () => {
+  it("renders without crashing", () => {
+    render(<Display />);
+  });
+
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("by default renders closed and locked to be false", () => {
+    const { getByText } = render(<Display />);
+    getByText(/unlocked/i);
+    getByText(/open/i);
+  });
+
+  it("by default locked class is `green-led`", () => {});
+});
