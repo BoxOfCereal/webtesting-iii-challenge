8e6afc45bc39cce0752f5ebac4ac25b546ebe558
unknown <noah.stoeckel@gmail.com>
2019-04-16 15:35:28 -0400

    aad snapshot and render test to Controls.spec.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "react-testing-library";
+
+import Controls from "./Controls";
+
+describe("<Controls/>", () => {
+  it("renders without crashing", () => {
+    render(<Controls />);
+  });
+
+  it("should match snapshot", () => {
+    const tree = renderer.create(<Controls />); // generates a DOM tree
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
